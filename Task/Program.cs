e0de0781562f952d1f31295b94145f0c2cb9d898
ritesu <ritesu@mail.ru>
2023-02-28 16:40:32 +1100

    FillArray

---
+++ Task/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите количество даных в массиве");
+int a = int.Parse(Console.ReadLine()??"");
+string[] arr = new string [a];
+Console.WriteLine("Заполните массив");
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Console.ReadLine()??"";
+}
